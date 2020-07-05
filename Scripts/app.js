201875a5d9cf72349eb50591964f83cfb41740dd
Duy Hieu Nguyen <phapsuaogiac@gmail.com>
2020-07-05 19:31:11 -0400

    Added second function

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,10 +2,34 @@
 // IIFE - Immediately Invoked Function Expression = anonymous self-executing function - closure
 (function(){
 
+    
+   
+
+    //nameed function
     function Start()
     {
-        console.log("App Started...");
+       
+        console.log('%cApp Started...', "color:blue; font-size: 24px;");
+    
+        Second();
     }
+
+    function Second()
+    {
+        console.log("This is second function");
+    }
+    
+
+    //declarative format
+   /*  let start=function()
+    {
+        console.log("App Started...");
+    } */
+
+    /* let Start=()=>
+    {
+        console.log("App Started...");
+    } */
 
     window.addEventListener("load", Start);
 
