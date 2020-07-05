10ef0aeaa2b8704540c021ca4c772286a04e7708
Duy Hieu Nguyen <phapsuaogiac@gmail.com>
2020-07-05 05:17:16 -0400

    Added Main Navigation

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+// IIFE - Immediately Invoked Function Expression = anonymous self-executing function - closure
+// IIFE - Immediately Invoked Function Expression = anonymous self-executing function - closure
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
